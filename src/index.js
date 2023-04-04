b19f793728641777884cd372dbbf42871def8e71
josephcruel <Joseph-Cruel@live.nmit.ac.nz>
2023-04-05 11:05:34 +1200

    Made a function to find large numbers in a 2-D Array

--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
-arrayOfStuff = [1, 2, 3, 4,'five', 5, 6, 7, 8, 9, 10,'one','two','three','four','six','seven','eight','nine','ten'];
+// arrayOfStuff = [1, 2, 3, 4,'five', 5, 6, 7, 8, 9, 10,'one','two','three','four','six','seven','eight','nine','ten'];
 
-function removeStrings(arr){
-    let listOfNumbers = []
-    for (let i = 0; i < arr.length; i++) {
-        if (typeof arr[i] == 'number') {
-            listOfNumbers.push(arr[i])
-        }
+// function removeStrings(arr){
+//     let listOfNumbers = []
+//     for (let i = 0; i < arr.length; i++) {
+//         if (typeof arr[i] == 'number') {
+//             listOfNumbers.push(arr[i])
+//         }
     
+//     }
+//     return listOfNumbers
+// }
+
+// console.log(removeStrings(arrayOfStuff))
+
+
+// //A different solution 
+
+// function arrayNumber(arr) {
+//     return arr.filter(i => typeof i == 'number')
+// }
+
+// console.log(arrayNumber([1,2, "Joseph", 4,5, "Caleb"]))
+
+
+myArray = [[1,2,3],[4,5,6],[10,20,100]];
+
+function largeNumber(arr) {
+    let outputArray = []
+    for(let i = 0; i < arr.length; i++) {
+        outputArray.push (Math.max(...arr[i]))
     }
-    return listOfNumbers
+    return outputArray
 }
-
-console.log(removeStrings(arrayOfStuff))
-
-
-//A different solution 
-
-function arrayNumber(arr) {
-    return arr.filter(i => typeof i == 'number')
-}
-
-console.log(arrayNumber([1,2, "Joseph", 4,5, "Caleb"]))
+console.log(largeNumber(myArray));
