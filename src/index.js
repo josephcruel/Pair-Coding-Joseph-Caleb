a2ed9ab3449afbe3c980dc7a9634d6cdc6f6b053
Calzep <caleb-easpn@nmit.live.ac.nz>
2023-04-05 10:34:18 +1200

    Created function to remove strings from array

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-//index.js developer branch
+arrayOfStuff = [1, 2, 3, 4,'five', 5, 6, 7, 8, 9, 10,'one','two','three','four','six','seven','eight','nine','ten'];
+
+function removeStrings(arr){
+    let listOfNumbers = []
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] == 'number') {
+            listOfNumbers.push(arr[i])
+        }
+    
+    }
+    return listOfNumbers
+}
+
+console.log(removeStrings(arrayOfStuff))
